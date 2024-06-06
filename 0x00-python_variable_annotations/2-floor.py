cc1e3133106e2bb1b402d9346b15641cd1137bc8
Shaker Sharabi <sheko100@icloud.com>
2024-06-06 23:38:20 +0300

    Add 2-floor.py file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that defines function floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Reduce the float number to an integer number"""
+    return math.floor(n)
