53bd9dcd103101ec82d3a88ba7e16a3f9c0e9b30
Shaker Sharabi <sheko100@icloud.com>
2024-06-07 01:46:07 +0300

    Add 100-safe_first_element.py file

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that defines function safe_first_element with type annotations
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Checks if the argument is defined before accessing it"""
+    if lst:
+        return lst[0]
+    else:
+        return None
