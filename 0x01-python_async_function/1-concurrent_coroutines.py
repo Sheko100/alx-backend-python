93859704afed53e6b5a396338a69d8a0285f8e45
Shaker Sharabi <sheko100@icloud.com>
2024-06-11 05:56:44 +0300

    Add 1-concurrent_coroutines.py file

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module defines wait_n function
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Calls the wait_random function n times"""
+    wait_list: List[float] = []
+    highest: float = 0.0
+    i: int = 0
+
+    for i in range(0, n):
+        wait = await wait_random(max_delay)
+        wait_list.append(wait)
+
+    i = 0
+    while i < len(wait_list):
+        if len(wait_list) - 1 != i and wait_list[i] > wait_list[i + 1]:
+            tmp: float = wait_list[i]
+            wait_list[i] = wait_list[i + 1]
+            wait_list[i + 1] = tmp
+        i += 1
+
+    return wait_list
