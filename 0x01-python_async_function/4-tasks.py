94a252f23b320afc345a458a1b1c907ce31a56d4
Shaker Sharabi <sheko100@icloud.com>
2024-06-12 05:52:43 +0300

    Add 4-tasks.py file

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -14,31 +14,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
         wait = await task_wait_random(max_delay)
         wait_list.append(wait)
 
-    return asc_sort(wait_list)
-
-
-def asc_sort(lst: List):
-    """Sorts a list to be in ascending order"""
-    srtd_lst: List = lst[:]
-    ri: int = 0
-    le: int = 0
-    lst_len: int = len(lst)
-
-    while ri < lst_len - 1:
-        if srtd_lst[ri] > srtd_lst[ri+1]:
-            tmp = srtd_lst[ri]
-            srtd_lst[ri] = srtd_lst[ri+1]
-            srtd_lst[ri+1] = tmp
-
-            le = ri
-            while le > 0:
-                if srtd_lst[le] < srtd_lst[le-1]:
-                    tmp = srtd_lst[le]
-                    srtd_lst[le] = srtd_lst[le-1]
-                    srtd_lst[le-1] = tmp
-                else:
-                    break
-                le -= 1
-        ri += 1
-
-    return srtd_lst
+    return sorted(wait_list.copy())
