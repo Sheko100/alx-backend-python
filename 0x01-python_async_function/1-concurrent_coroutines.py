19a0e66afda07803b736b1505bfbb8931abe6c2a
Shaker Sharabi <sheko100@icloud.com>
2024-06-12 04:29:41 +0300

    Add 1-concurrent_coroutines.py file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,24 +9,38 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Calls the wait_random function n times"""
     wait_list: List[float] = []
-    highest: float = 0.0
-    i: int = 0
 
-    for i in range(0, n):
+    for i in range(n):
         wait = await wait_random(max_delay)
         wait_list.append(wait)
 
-    i = 0
-    while i < len(wait_list):
-        if len(wait_list) - 1 != i and wait_list[i] > wait_list[i + 1]:
-            tmp: float = wait_list[i]
-            wait_list[i] = wait_list[i + 1]
-            wait_list[i + 1] = tmp
-        elif i - 1 >= 0 and wait_list[i - 1] > wait_list[i]:
-            tmp: float = wait_list[i]
-            wait_list[i] = wait_list[i - 1]
-            wait_list[i - 1] = tmp
-
-        i += 1
+    wait_list = asc_sort(wait_list)
 
     return wait_list
+
+
+def asc_sort(lst: List):
+    """Sorts a list to be in ascending order"""
+    srtd_lst: List = lst
+    ri: int = 0
+    le: int = 0
+    lst_len: int = len(lst)
+
+    while ri < lst_len - 1:
+        if srtd_lst[ri] > srtd_lst[ri+1]:
+            tmp = srtd_lst[ri]
+            srtd_lst[ri] = srtd_lst[ri+1]
+            srtd_lst[ri+1] = tmp
+
+            le = ri
+            while le > 0:
+                if srtd_lst[le] < srtd_lst[le-1]:
+                    tmp = srtd_lst[le]
+                    srtd_lst[le] = srtd_lst[le-1]
+                    srtd_lst[le-1] = tmp
+                else:
+                    break
+                le -= 1
+        ri += 1
+
+    return srtd_lst
