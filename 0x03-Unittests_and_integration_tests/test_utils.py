0ff1f5bea2f289c4d81751c7e3303b79b2f4ea98
Shaker Sharabi <sheko100@icloud.com>
2024-06-30 00:54:13 +0300

    Add test_utils.py file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,6 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
             nested_map: Dict,
             path: Sequence,
             expected: Union[int, Dict]
-            ) -> None:
-        """Testing that access_nested_map returns the expected value"""
+            ):
+        """Testing that access_nested_map returns the expected value
+        """
         self.assertEual(access_nested_map(nested_map, path), expected)
