787c1e65e15fe0902b15419fe34e3f93e8f7f1bd
Shaker Sharabi <sheko100@icloud.com>
2024-06-30 00:40:44 +0300

    Add test_utils.py file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
+from typing import Dict, Sequence, Union
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,6 +16,12 @@ class TestAccessNestedMap(unittest.TestCase):
         ("short path", {"a": {"b": 2}}, ("a", ), {"b": 2}),
         ("whole path", {"a": {"b": 2}}, ("a", "b", ), 2)
         ])
-    def test_access_nested_map(self, name, nested_map, path, expected):
+    def test_access_nested_map(
+            self,
+            name: str,
+            nested_map: Dict,
+            path: Sequence,
+            expected: Union[int, Dict]
+            ):
         """Testing that access_nested_map returns the expected value"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
