8e17c37854c40a96180e2d9000cd4c9f362a112e
Shaker Sharabi <shakersharabi100@gmail.com>
2024-12-19 10:00:18 +0200

    Add test_utils.py file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,10 +19,10 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parametrized.expand([
+    @parameterized.expand([
         ({}, ("a", )),
         ({"a": 1}, ("a", "b"))
-    })
+    ])
     def test_access_nested_map_exception(self, nested_map, path):
         """Tests that the exception is raising if path is wrong"""
         with self.assertRaises(KeyError):
