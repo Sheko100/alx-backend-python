5b7256ff21f1f6a428b9fe6800ce453d106ac7da
Shaker Sharabi <shakersharabi100@gmail.com>
2024-12-21 14:53:51 +0200

    Add test_utils.py file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ from unittest.mock import patch
 from parameterized import parameterized
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -47,3 +48,26 @@ class TestGetJson(unittest.TestCase):
             res.json.return_value = expected
             self.assertEqual(get_json(url), expected)
             mock.assert_called()
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the memoize function
+    """
+
+    def test_memoize(self):
+        """Tests the memoize function normal behavior
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42):
+            newObj = TestClass()
+            self.assertEqual(newObj.a_property, 42)
+            self.assertEqual(newObj.a_property, 42)
+            newObj.a_method.assert_called_once()
