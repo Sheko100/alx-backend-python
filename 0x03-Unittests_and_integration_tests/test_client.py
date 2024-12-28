82acda319132643017d723b2f3ba2c7e17edc14a
Shaker Sharabi <shakersharabi100@gmail.com>
2024-12-28 22:36:20 +0200

    Add test_client.py file

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module for unit testing
+"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests the githubOrgCLient class
+    """
+
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+        ])
+    @patch('client.get_json')
+    def test_org(self, org, mock):
+        """Tests the org method
+        """
+        client = GithubOrgClient(org)
+        url = client.ORG_URL.format(org=org)
+        client.org
+        mock.assert_called_once_with(url)
