b3f452be46b5b4e76c607453217b90fdb2e4b062
Shaker Sharabi <shakersharabi100@gmail.com>
2024-12-29 20:50:28 +0200

    Add test_client.py file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,13 +34,14 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(client._public_repos_url, expected_url)
 
     @patch('client.get_json')
-    def test_public_repos(self, mock_get_json);
+    def test_public_repos(self, mock_get_json):
         """Tests public_repos method
         """
         with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_repos_url:
-            repos_url = 'https://public_repos_url.com'
-            mock_get_json.return_value = {{'name': 'a1'}, {'name': 'a2'}}
-            mock_repos_url.return_value = repos_url
+            mock_get_json.return_value = [{'name': 'a1'},{'name': 'a2'}]
+            mock_repos_url.return_value = 'https://public_repos_url.com'
             client = GithubOrgClient('cool')
-            print(client.public_repos(license=None))
+            self.assertEqual(client.public_repos(license=None), ['a1', 'a2'])
+            mock_get_json.assert_called_once()
+            mock_repos_url.assert_called_once()
 
