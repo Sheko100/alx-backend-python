d176b4648c6867a497aa6a6bf3b4fdc85e2389cc
Shaker Sharabi <shakersharabi100@gmail.com>
2024-12-29 17:08:47 +0200

    Add test_client.py file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Module for unit testing
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -23,3 +23,12 @@ class TestGithubOrgClient(unittest.TestCase):
         url = client.ORG_URL.format(org=org)
         client.org
         mock.assert_called_once_with(url)
+
+    def test_public_repos_url(self):
+        """Tests _public_repos_url method
+        """
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+            expected_url = 'https://example.com'
+            mock_org.return_value = {"repos_url": expected_url}
+            client = GithubOrgClient('cool')
+            self.assertEqual(client._public_repos_url, expected_url)
